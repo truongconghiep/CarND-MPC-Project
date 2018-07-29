c10bd9a0d57aed3bc842fdcb6da47b995a42182f
Hiep Truong <truongconghiep@gmail.com>
2018-07-30 00:04:58 +0200

    rework max speed 74 mph

--- src/MPC.h
+++ src/MPC.h
@@ -6,6 +6,14 @@
 
 #define LF (float)2.67
 
+/* MPC cost weights */
+#define CTE_COST_WEIGHT                 (double)2000
+#define EPSI_COST_WEIGHT                (double)2000
+#define DELTA_COST_WEIGHT_PROPORTIONAL  (double)30
+#define A_COST_WEIGHT_PROPORTIONAL      (double)30
+#define DELTA_COST_WEIGHT_DIFFERENT     (double)4000000
+#define A_COST_WEIGHT_DIFFERENT         (double)20000
+
 typedef unsigned int uint_t;
 
 using namespace std;
