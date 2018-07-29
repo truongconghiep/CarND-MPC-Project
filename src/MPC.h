11619d7c0d9c0acf85f5c8f1b3d0041e10467f47
Hiep Truong <truongconghiep@gmail.com>
2018-07-30 00:46:55 +0200

    reduce max speed to 71 mph to make sure no accident occurs

--- src/MPC.h
+++ src/MPC.h
@@ -9,9 +9,9 @@
 /* MPC cost weights */
 #define CTE_COST_WEIGHT                 (double)2000
 #define EPSI_COST_WEIGHT                (double)2000
-#define DELTA_COST_WEIGHT_PROPORTIONAL  (double)30
-#define A_COST_WEIGHT_PROPORTIONAL      (double)30
-#define DELTA_COST_WEIGHT_DIFFERENT     (double)4000000
+#define DELTA_COST_WEIGHT_PROPORTIONAL  (double)35
+#define A_COST_WEIGHT_PROPORTIONAL      (double)35
+#define DELTA_COST_WEIGHT_DIFFERENT     (double)5000000
 #define A_COST_WEIGHT_DIFFERENT         (double)20000
 
 typedef unsigned int uint_t;
