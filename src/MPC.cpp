a87885c833c7b945078fe4a4746b61aca79735bb
Hiep Truong <truongconghiep@gmail.com>
2018-07-29 22:59:05 +0200

    max speed 77 mph

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -64,22 +64,22 @@ class FG_eval
     /* The part of the cost based on the reference state.*/
     for (unsigned int t = 0; t < N; t++) 
 	{
-      fg[0] += 1000 * CppAD::pow(vars[cte_start + t] - ref_cte, 2);
-      fg[0] += 1000 * CppAD::pow(vars[epsi_start + t] - ref_epsi, 2);
+      fg[0] += 2000 * CppAD::pow(vars[cte_start + t] - ref_cte, 2);
+      fg[0] += 2000 * CppAD::pow(vars[epsi_start + t] - ref_epsi, 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     /* Minimize the use of actuators.*/
     for (unsigned int t = 0; t < N - 1; t++) 
 	{
-      fg[0] += 30 * CppAD::pow(vars[delta_start + t], 2);
-      fg[0] += 30 * CppAD::pow(vars[a_start + t], 2);
+      fg[0] += 25 * CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 25 * CppAD::pow(vars[a_start + t], 2);
     }
 
     /* Minimize the value gap between sequential actuations.*/
     for (unsigned  int t = 0; t < N - 2; t++) 
 	{
-      fg[0] += 3000000 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 4000000 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += 20000 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
